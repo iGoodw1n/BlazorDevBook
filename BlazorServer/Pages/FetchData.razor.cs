119f03ff108e5437cb91ffae751b173e3d265eee
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-12-20 18:52:26 +0400

    Change FetchData page to Code behind

---
+++ BlazorServer/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using BlazorServer.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Pages;
+
+public partial class FetchData
+{
+    private WeatherForecast[]? forecasts;
+
+    [Inject]
+    public WeatherForecastService? ForecastService { get; set; }
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (ForecastService is not null)
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now));
+        }
+    }
+}
