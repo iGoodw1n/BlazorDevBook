35fb4431e492d78d282d6305f68459294d377c6a
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-12-22 02:01:14 +0400

    Add Categories Api

---
+++ BlazorWebAssembly/Server/Endpoints/CategoryEndpoints.cs
@@ -0,0 +1,32 @@
+﻿using Data.Models;
+using Data.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWebAssembly.Server.Endpoints;
+
+public static class CategoryEndpoints
+{
+    public static void MapCategory(this WebApplication app)
+    {
+        app.MapGet("/api/Categories", async (IBlogApi api) =>
+        {
+            return Results.Ok(api.GetCategoriesAsync());
+        });
+
+        app.MapGet("/api/Categories/{*id}", async (IBlogApi api, string id) =>
+        {
+            return Results.Ok(api.GetCategoryAsync(id));
+        });
+
+        app.MapDelete("/api/categories{*id}", async (IBlogApi api, string id) =>
+        {
+            await api.DeleteCategoryAsync(id);
+            return Results.Ok();
+        });
+
+        app.MapPut("/api/Categories", async (IBlogApi api, [FromBody] Category item) =>
+        {
+            return Results.Ok(await api.SaveCategoryAsync(item));
+        });
+    }
+}
