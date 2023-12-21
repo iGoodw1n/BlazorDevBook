54f97ab5fe134cc6d1e2cb0a498bcb84a03601ac
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2023-12-21 13:34:20 +0400

    Add bootstrap css class component

---
+++ Components/RazorComponents/BootstrapFieldCssClassProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Components.RazorComponents
+{
+    public class BootstrapFieldCssClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+            var isModified = editContext.IsModified(fieldIdentifier);
+            return (isModified, isValid) switch
+            {
+                (true, true) => "form-contorl modified is-valid",
+                (true, false) => "form-control modified is-invalid",
+                (false, true) => "form-control",
+                (false, false) => "form-control"
+            };
+        }
+    }
+}
